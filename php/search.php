6ac358c119d9ac5ba3610b40e2b300fb5335175b
Jeremy Mekker <jeremymekker@gmail.com>
2022-06-08 17:00:40 -0700

    Fixed country search field, fixed basic search input, and fixed advanced search input

--- php/search.php
+++ php/search.php
@@ -10,9 +10,8 @@
 	<div class="list-container">
 		<button onclick="history.go(-1);" class="back">Back</button>
 		<?php
-            $sql = "SELECT * FROM NGO WHERE ";
+            $sql = "SELECT * FROM NGO WHERE ngo_name LIKE ''";
 
-            
                     // Setting the searchbox into the search query
                 if (isset($_POST['submit-search'])) {
                     if (isset($_POST['searchbox1'])) {
@@ -21,33 +20,48 @@
                     if (isset($_POST['searchbox2'])) {
                         $search = mysqli_real_escape_string($conn, $_POST['searchbox2']);
                     }
-                    $sql .= "ngo_name like '%$search%' OR website LIKE '%$search%'";
-
+                    if ($search != null) {
+                        $sql .= " OR ngo_name LIKE '%$search%' OR website LIKE '%$search%'";
+                    }
 
                     // Setting country into the search query
                     if (isset($_POST['country'])) {
                         $country = $_POST['country'];
-                        $sql .= " OR country LIKE '%$country%'";
+                        if ($country != 0) {
+                            // if selector is not on default, check for the name of the  country with countryID
+                            $requestCountry = "SELECT * FROM countries WHERE countryID = '$country'";
+                            $selectedCountry = mysqli_query($conn, $requestCountry);
+                            $resultCountry = mysqli_fetch_assoc($selectedCountry);
+                            // and add the country name to search query
+                            $sql .= " OR country = '$resultCountry[countryName]'";
+                        }
                     }
-
 
                     // Setting SDGs into the search query
                     if (isset($_POST['sdg'])) {
                         $sdg = $_POST['sdg'];
-                        $N = count($sdg);
-                        $sdgsChosen = "";
-                        for ($i = 0; $i < $N; $i++) {
-                            $sdgsChosen = $sdgsChosen . $sdg[$i] . " ";
+                        if ($sdg != null) {
+                            $N = count($sdg);
+                            $sdgsChosen = "";
+                            for ($i = 0; $i < $N; $i++) {
+                                $sdgsChosen = $sdgsChosen . $sdg[$i] . " ";
+                            }
+                            $sql .= " OR tags LIKE '%$sdgsChosen%' OR ngo_description LIKE '%$sdgsChosen%'";
+                        } else {
+                            if ($search != null) {
+                                $sql .= " OR tags LIKE '%$search%'";
+                            }
                         }
-                        $sql .= " OR tags LIKE '%$sdgsChosen%'";
-                    } else {
-                        $sql .= " OR tags LIKE '%$search%'";
                     }
 
-
-                    $sql .= " OR ngo_description like '%$search%' OR ngo_description LIKE '%$sdgsChosen%' OR ngo_description LIKE '%$country%';";
-                    // $sql .= ";";
-                    echo $sql;
+                    // fall-back search only shows active organizations
+                    if ($sql == "SELECT * FROM NGO, countries WHERE ngo_name LIKE ''") {
+                        $sql .= " OR active = '1'";
+                    }
+                    
+                    // This is where you need to apply pearson-correlation
+                    $sql .= " ORDER BY entry_id;";
+                    
                     $result = mysqli_query($conn, $sql);
                     $queryResult = mysqli_num_rows($result);
                     if ($queryResult == 1) {
@@ -59,14 +73,14 @@
                     }
                     if ($queryResult > 0) {
                         $row = mysqli_fetch_assoc($result);
-                        while ($row) {
+                        foreach ($result as $entry) {
                             echo "<div class='ngo-box'><br>
-						    <a href='ngo.php?t=" . $row['entry_id'] ."'>
-						    <h4>" . $row['ngo_name'] . "</h4></a><br>
-						    <p>" . $row['ngo_description'] . "</p><br>
-						    <p>Tags:</p>
-						    <p>" . $row['tags'] . "</p>
-						    </div>";
+                            <a href='ngo.php?t=" . $entry['entry_id'] ."'>
+                            <h4>" . $entry['ngo_name'] . "</h4></a><br>
+                            <p>" . $entry['ngo_description'] . "</p><br>
+                            <p>Tags:</p>
+                            <p>" . $entry['tags'] . "</p>
+                            </div>";
                         }
                     }
                 }
